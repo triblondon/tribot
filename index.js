56192fa2cc3e47205b04b7bda3274c0c83ade1c6
Andrew Betts <andrew.betts@ft.com>
2015-04-11 11:18:21 +0100

    Linting

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 if (!require('fs').existsSync('./config.json')) {
-	console.error('Missing config file.  Before you start tribot, you need to configure it by creating a config.json file.  See the README for more details.')
+	console.error('Missing config file.  Before you start tribot, you need to configure it by creating a config.json file.  See the README for more details.');
 	process.exit(1);
 }
 
