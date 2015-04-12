4d6ad7f1a52d6839329bde50e961de6cdd21f24c
Andrew Betts <andrew.betts@ft.com>
2015-04-12 15:49:47 +0100

    Support time zones

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+var config;
+
 if (!require('fs').existsSync('./config.json')) {
 	console.error('Missing config file.  Before you start tribot, you need to configure it by creating a config.json file.  See the README for more details.');
 	process.exit(1);
+} else {
+	config = require('./config.json');
 }
 
+// Patch the global Date object to support time zones and set the time zone correctly
+// Since the effect is global this is done outside of the Bot class
+var time = require('time')(Date);
+if ("timeZone" in config) time.tzset(config.timeZone);
 
 var Bot = require('./lib/bot');
 var b = new Bot(require('./config.json'));
