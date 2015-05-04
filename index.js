b784dfd729da84724a6c06aadfd3c76467e64d3c
Andrew Betts <andrew.betts@ft.com>
2015-05-04 13:44:58 +0100

    Partial refactor using ES6 class (untested)

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var config;
+require("babel/register");
 
-if (!require('fs').existsSync('./config.json')) {
+try {
+	const config = require('./config.json');
+} catch (e) {
 	console.error('Missing config file.  Before you start tribot, you need to configure it by creating a config.json file.  See the README for more details.');
 	process.exit(1);
-} else {
-	config = require('./config.json');
 }
 
 // Patch the global Date object to support time zones and set the time zone correctly
@@ -13,6 +13,6 @@ var time = require('time')(Date);
 if ("timeZone" in config) time.tzset(config.timeZone);
 
 var Bot = require('./lib/bot');
-var b = new Bot(require('./config.json'));
+var b = new Bot(config);
 
 b.connect();
